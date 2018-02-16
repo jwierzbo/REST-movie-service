9f3390d187bd5be9d7f5f5ab4bb5051ab5a67322
jwierzbo <jakubwierzbowski@gmail.com>
2018-02-16 10:37:11 +0100

    Tomcat config

--- README.md
+++ README.md
@@ -6,12 +6,18 @@ REST CRUD Example for Spring Boot
     ```shell
     $ mvn spring-boot:run
     ```
-1. To build and run from executable jar:
+1. To build and run from executable war:
     ```shell
     $ mvn package
-    $ java -jar target/REST-movie-service-0.1.jar
+    $ java -jar target/movies-0.3.war
+    ```
+1. To build and run on standalone Tomcat server:
+    ```shell
+    $ mvn package -Ptomcat
+    $ cp target/movies-0.3.war <Tomcat Root>/webapps/
     ```
 1. To access Movies app, open: <http://localhost:8081/v1/movies>
+1. To access Movies app deployed on Tomcat: <http://localhost:8080/movies-0.3/v1/movies>
 1. To access swagger-ui, open: <http://localhost:8081/swagger-ui.html>
 
 
--- src/main/java/net/jwierzbo/rest/Application.java
+++ src/main/java/net/jwierzbo/rest/Application.java
@@ -2,11 +2,20 @@ package net.jwierzbo.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
+    /* Spring Boot entry point*/
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    /*Standalone server entry point (e.g. Tomcat)*/
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
     }
 }
